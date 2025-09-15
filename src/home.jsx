89f4e1b3de941ef455c0c25fbdfe795504b82207
Emzzy241 <emzzyoluwole@gmail.com>
2025-09-15 12:45:23 +0100

    add header component for frontend

---
+++ src/home.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+export default function Home () {
+    return (
+        <div>
+            {/* Navigation section, use useState for responsiveness */}
+            <Header />
+            {/* hero section */}
+        </div>
+    );
+}
