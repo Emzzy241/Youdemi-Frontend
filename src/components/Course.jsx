6701cb36735702adbb6f16a903fe8e2c946235d7
Emzzy241 <emzzyoluwole@gmail.com>
2026-03-31 13:54:35 +0100

    add Course() component

---
+++ src/components/Course.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Course() {
+    return (
+        <React.Fragment>
+            <h2>Hey there, welcome to the /courses route.</h2>
+        </React.Fragment>
+    )
+}
+
+export default Course;
