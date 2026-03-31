5524d919a8bb53c80fbf63ea738ca1a88d4804d1
Emzzy241 <emzzyoluwole@gmail.com>
2026-03-31 16:35:58 +0100

    add utilities

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+// src/utils/constants.js
+export const COURSE_CATEGORIES = {
+  'programming-tech': { label: 'Programming & Tech', dbTags: ['programming', 'tech'] },
+  'tech': { label: 'Tech', dbTags: ['tech'] },
+  'data-science': { label: 'Data Science', dbTags: ['data-science'] },
+  
+  // Notice these keys match your Navbar Hrefs exactly now:
+  'management-company': { label: 'Management', dbTags: ['management', 'leadership'] },
+  'management': { label: 'Management', dbTags: ['management', 'leadership'] },
+  'startup-company': { label: 'Startup & Business', dbTags: ['startup', 'business', 'finance'] },
+  'startup': { label: 'Startup & Business', dbTags: ['startup', 'business', 'finance'] },
+  'arts-humanities': { label: 'Arts & Humanities', dbTags: ['arts', 'humanities'] }
+};
