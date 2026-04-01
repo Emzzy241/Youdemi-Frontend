e8738ae9236b593429887267ee10157e5f79a101
Emzzy241 <emzzyoluwole@gmail.com>
2026-04-01 10:07:30 +0100

    update App() component

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
         <Route path="/dashboard" element={
           <ProtectedRoute>
             <Dashboard />
-
           </ProtectedRoute>
         } />
         <Route path="/AccountVerificationRequest" element={<AccountVerificationRequest />} />
